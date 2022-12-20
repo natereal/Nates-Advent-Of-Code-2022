39e2eceea1273e8b8d547b9989e39242c320f1c5
Nathan Doyle <Nathan.Doyle@agriculture.gov.ie>
2022-12-20 15:11:48 +0000

    Solution day 20 using arrays and modulo arithmetic

--- R/draft_day20.R
+++ R/draft_day20.R
@@ -1,9 +1,18 @@
+library(tidyverse)
+
 test <- c("1", "2", "-3", "3", "-2", "0", "4")
 test <- as.numeric(test)
 
 # Mod is length - 1
-m <- function(n, mod = 6) {
-    ((n-2) %% mod) + 2
+#' For N,
+#' 0,1,2,...,N-1
+modulo <- function(n, mod = 6) {
+    n %% mod
+}
+#' Adjusts modulo for indexes starting from 1 i.e
+#' For N, it is 1,2,3,...,N
+adjust <- function(n, mod) {
+    ((n-1) %% mod) + 1
 }
 
 insert <- function(vec, value, pos) {
@@ -16,96 +25,38 @@ insert <- function(vec, value, pos) {
     return(out)
 }
 
-pop <- function(vec) {
-    return(tail(vec, -1))
-}
-
 x <- test
 x <- readLines("inst/input20.txt") |> as.numeric()
 I <- length(x)
 df <- tibble::tibble(values = x, move_order = 1:I)
 
-for(i in 1:I) {
-    # i <- min(which(df$to_be_checked))
-    # backwards_adjustment <- ifelse(df$values[i] < 0, -1, 0)
-    # target_index <- m(i + df$values[i] + backwards_adjustment) # move forward number of positions
+decrypt <- 811589153
+df$values <- df$values * decrypt
+TIMES <- 10
+for(T in 1:TIMES) {
+    for(i in 1:I) {
+        val   <- df$values[df$move_order == i]
+        current_index <- which(df$move_order == i)
 
-    val   <- df$values[df$move_order == i]
-    current_index <- which(df$move_order == i)
-    backwards_adjustment <- ifelse(val < 0, -1, 0)
-    target_index <- m(current_index + val + backwards_adjustment, mod = I-1)
+        # To handle moving backwards, just take modulo N-1
+        movement <- modulo(val, I-1)
 
-    df$values <- insert(df$values[-current_index], val, target_index)
-    df$move_order <- insert(df$move_order[-current_index], 0, target_index)
-    # print(df)
+        # Insert 1 is equivalent to Insert N
+        # So the insertion point is modulo N-1, but starting from 1,
+        # (R indexes start at 1)
+        # The insertion point is current index + number of movements
+        target_index <- adjust(current_index + movement, mod = I-1)
+
+        df$values <- insert(df$values[-current_index], val, target_index)
+        df$move_order <- insert(df$move_order[-current_index], i, target_index)
+        # print(df)
+    }
 }
 df
 
-adjust <- function(n, mod) {
-    ((n-1) %% mod) + 1
-}
-
 zero <- which(df$values == 0)
-c1 <- adjust(zero + 1000, I)
-c2 <- adjust(zero + 2000, I)
-c3 <- adjust(zero + 3000, I)
-
-sum(df$values[c(c1, c2, c3)])
-
-#
-v <- df$values
-
-i = 1
-v
-val <- 100
-v <- insert(v, val, m(i + val))
-if(v[i] == val) {
-    v <- v[-i]
-}
-
-v
-# When i = 1, then pos is i + moves_forward
-insert(v[-i], val, 1)
-insert(v[-i], val, 2)
-insert(v[-i], val, 3)
-insert(v[-i], val, 4)
-insert(v[-i], val, 5)
-insert(v[-i], val, 6)
-insert(v[-i], val, 7)
-
-# When i = 1, then pos is i + moves_forward
-i = 2
-v
-insert(v[-i], val, 1)
-insert(v[-i], val, 2)
-insert(v[-i], val, 3)
-insert(v[-i], val, 4)
-insert(v[-i], val, 5)
-insert(v[-i], val, 6)
-insert(v[-i], val, 7)
-
-i = 6
-v
-insert(v[-i], 100, 1) # +1, and shift
-insert(v[-i], 100, 2) # +2
-insert(v[-i], 100, 3) # +3
-insert(v[-i], 100, 4) # Somehow this is move +4
-insert(v[-i], 100, 5) # +5
-insert(v[-i], 100, 6) # Move 0 or Move +6?
-insert(v[-i], 100, 7) # Move +1
-
-#' 1 - Exclude
-#' 2 - 2
-#' 3 - 3
-#' 4 - 4
-#' 5 - 5
-#' 6 - 0/6
-#' 7 - 1/7
-m(1)
-m(2)
-m(3)
-m(4)
-m(5)
-m(6)
-m(7)
-m(8)
+c1 <- df$values[adjust(zero + 1000, mod = I)]
+c2 <- df$values[adjust(zero + 2000, mod = I)]
+c3 <- df$values[adjust(zero + 3000, mod = I)]
+options(scipen = 20)
+sum(c1 + c2 +c3)
